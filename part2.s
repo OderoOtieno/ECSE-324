d09f9e5c055afd5fbca4c581ac87688227894c6f
Tyrone Wong <Ty-Won@users.noreply.github.com>
2018-09-25 17:57:27 -0400

    Create part2.s

---
+++ part2.s
@@ -0,0 +1,48 @@
+			.text
+			.global _start
+
+_start:
+			LDR R4, =MAXRESULT
+			LDR R2, [R4, #4]
+			ADD R3,R4, #8
+			LDR R0, [R3]
+			LDR R5, =MINRESULT
+
+			
+LOOP1:		SUBS R2, R2, #1
+			BEQ STORE
+			ADD R3, R3, #4
+			LDR R1, [R3]
+			CMP R0, R1
+			BGE LOOP1
+			MOV R0, R1
+			B LOOP1
+
+STORE: 		STR R0, [R4]
+			LDR R0, [R4]
+
+			LDR R2, [R4, #4]
+			ADD R3,R4, #8
+			LDR R0, [R3]
+			B LOOP2
+
+LOOP2:		SUBS R2, R2, #1
+			BEQ DONE
+			ADD R3, R3, #4
+			LDR R1, [R3]
+			CMP R0, R1
+			BLE LOOP2
+			MOV R0, R1
+			B LOOP2
+
+DONE:		STR R0, [R5]
+			SUBS R4, R4,R5
+			LSR R4, R4,#2
+
+END:		B END
+
+MAXRESULT:		.word	0
+N:			.word	7
+NUMBERS:	.word	4, 5, 3, 6
+			.word	1, 8, 2
+MINRESULT:		.word	0
